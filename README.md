780f061459a13c78e91ea54d77d4bc6737b07f24
maslynem <maslynem@student.21-school.ru>
2023-08-24 10:01:28 +0300

    add Text Justification

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # LeetCode
-| # | Title | Solution                                             | Difficulty |
-|---| ----- |------------------------------------------------------| ---------- |
-|767|[Reorganize String](https://leetcode.com/problems/reorganize-string/) | [Java](src/main/java/ReorganizeString/Solution.java) |Medium|
+| #   | Title | Solution                                             | Difficulty |
+|-----| ----- |------------------------------------------------------|------------|
+| 767 |[Reorganize String](https://leetcode.com/problems/reorganize-string/) | [Java](src/main/java/ReorganizeString/Solution.java) | Medium     |
+| 68  |[Text Justification](https://leetcode.com/problems/text-justification/description/) | [Java](src/main/java/TextJustification/Solution.java) | Hard       |
---
+++ src/test/java/TextJustification/SolutionTest.java
@@ -0,0 +1,38 @@
+package TextJustification;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    private static final Solution solution = new Solution();
+
+    @Test
+    void test1() {
+        String[] input = {"This", "is", "an", "example", "of", "text", "justification."};
+        int maxWidth = 16;
+        List<String> result = Arrays.asList( "This    is    an", "example  of text", "justification.  ");
+
+        assertIterableEquals(result, solution.fullJustify(input, maxWidth));
+    }
+    @Test
+    void test2() {
+        String[] input = {"What","must","be","acknowledgment","shall","be"};
+        int maxWidth = 16;
+        List<String> result = Arrays.asList( "What   must   be","acknowledgment  ","shall be        ");
+
+        assertIterableEquals(result, solution.fullJustify(input, maxWidth));
+    }
+    @Test
+    void test3() {
+        String[] input = {"Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"};
+        int maxWidth = 20;
+        List<String> result = Arrays.asList("Science  is  what we","understand      well","enough to explain to","a  computer.  Art is","everything  else  we","do                  ");
+
+        assertIterableEquals(result, solution.fullJustify(input, maxWidth));
+    }
+
+}
